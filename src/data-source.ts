a7b391d352c1856fa1a417a395d3720921b3e572
snowmuffin <77647785+snowmuffin@users.noreply.github.com>
2025-09-04 15:28:45 +0900

    edit fallback

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ const dataSourceOptions: DataSourceOptions = {
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'hei_linkedin',
+  database: process.env.DB_NAME || 'postgres',
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: process.env.NODE_ENV === 'development',
